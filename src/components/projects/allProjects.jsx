a8f31ed382082275e058020beebed38d8c985797
Shivam-Singh <sd.shivam.00@gmail.com>
2024-11-11 18:02:00 +0000

    desc fix

--- src/components/projects/allProjects.jsx
+++ src/components/projects/allProjects.jsx
@@ -11,7 +11,7 @@ const AllProjects = () => {
 					<Project
 						logo={"https://github.com/Sd-Shiivam/Sd-Shiivam/blob/main/imagehost/Screenshot%20from%202024-10-17%2019-52-14.png?raw=true"}
 						title={project.name}
-						description={project.description}
+						description={project.description.substring(0, 100) + "....."}
 						linkText={project.full_name}
 						link={project.html_url}
 					/>
